c141c808d312def0664a9433684284bc7cf11c34
Phoenyxwastaken <Phoenyxwastaken@gmail.com>
2020-11-06 15:21:08 +0100

    implementing useState

--- src/App.js
+++ src/App.js
@@ -3,29 +3,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import { TextField } from "@material-ui/core";
 
-/*
-function App() {
-  const getColor = (e) => {
-    console.log(e.target.value);
-    document.body.style.backgroundColor = e.target.value;
-  };
-
-  return (
-    <div className="App">
-      <h1>Color Changer</h1>
-      <TextField label="Enter Color" type="string" onChange={getColor} />
-    </div>
-  );
-}
-*/
-
 function App() {
   const [empty, stateColor] = useState(undefined);
+  document.body.style.backgroundColor = stateColor;
+  useEffect(() => {
+    fetch("http://thecolorapi.com")
+      .then((response) => response.json())
+      .then((colors) => console.log(colors));
+  }, [stateColor]);
+  console.log();
 
   return (
     <div className="App">
       <h1>Color Changer</h1>
-      <TextField label="Enter Color" type="string" onChange={empty} />
+      <TextField label="Enter Color" type="string" onChange={stateColor} />
+      <p></p>
     </div>
   );
 }
