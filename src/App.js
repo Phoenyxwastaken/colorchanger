100b7d0386927032c9c8e8d0b70b44265c648881
Phoenyxwastaken <Phoenyxwastaken@gmail.com>
2020-11-06 15:37:31 +0100

    Co-authored-by: Tobias Zimmermann <zimmermann@sport-thieme.de>

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import "./App.css";
 import { TextField } from "@material-ui/core";
 
 function App() {
+  const [colorName, setColorName] = useState("");
   const [empty, stateColor] = useState(undefined);
   document.body.style.backgroundColor = stateColor;
+
   useEffect(() => {
-    fetch("http://thecolorapi.com")
+    fetch(`http://thecolorapi.com/id?hex=${stateColor}`)
       .then((response) => response.json())
-      .then((colors) => console.log(colors));
+      .then((colors) => setColorName(colors.name.value));
   }, [stateColor]);
 
   return (
     <div className="App">
       <h1>Color Changer</h1>
       <TextField label="Enter Color" type="string" onChange={stateColor} />
+      <br />
+      Die Farbe heißt: {colorName}
     </div>
   );
 }
